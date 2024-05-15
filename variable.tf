0f5ce63f77b9bd069bb416bbb7c813e83f4e2cf7
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-15 18:51:14 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,41 @@
+variable "subscription_id"{
+  type=string
+}
 
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "prefix"{
+ type=string
+ default="samiislam"
+}
+variable "firstname"{
+ type=string
+ default="sami"
+}
+variable "lastname"{
+ type=string
+ default="islam"
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "access_tier"{
+  type=string
+  default="Cool"
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
