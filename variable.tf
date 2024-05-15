51658a642ba1b678adf51121104fd1b1fcb84930
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-15 19:05:07 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -39,3 +39,13 @@ variable "cross_tenant_replication_enabled"{
  type=bool
  default=false
 }
+variable "administrator_login"{
+  type=string
+  default="konan"
+}
+
+variable "administrator_login_password"{
+  type=string
+  default="Dosint@2018"
+}
+ 
