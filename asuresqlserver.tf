6738ef258397249faa56a64abeeaab79df29adbe
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-20 12:07:54 -0400

    Update asuresqlserver.tf

--- asuresqlserver.tf
+++ asuresqlserver.tf
@@ -1,1 +1,23 @@
 
+resource "azurerm_resource_group" "mysqlresgroup" {
+  name     = "rs_mql_az"
+  location = "canadacentral"
+}
+
+resource "azurerm_mssql_server" "azuresqlserver" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.databaserg.name
+  location                     = azurerm_resource_group.databaserg.location
+  version                      = "12.0"
+ 
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
